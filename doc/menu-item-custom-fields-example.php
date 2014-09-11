03b2e83b549da7f970edab568a2506835eb0f1bd
Dzikri Aziz <kvcrvt@gmail.com>
2014-09-11 19:31:53 +0700

    Update example plugin

--- doc/menu-item-custom-fields-example.php
+++ doc/menu-item-custom-fields-example.php
@@ -4,19 +4,19 @@
  *
  * Copy this file into your wp-content/mu-plugins directory.
  *
- * @package Menu_Item_Custom_Fields
- * @version 0.1.0
+ * @package Menu_Item_Custom_Fields_Example
+ * @version 0.2.0
  * @author Dzikri Aziz <kvcrvt@gmail.com>
  *
  *
  * Plugin name: Menu Item Custom Fields Example
  * Plugin URI: https://github.com/kucrut/wp-menu-item-custom-fields
  * Description: Example usage of Menu Item Custom Fields in plugins/themes
- * Version: 0.1.0
+ * Version: 0.2.0
  * Author: Dzikri Aziz
  * Author URI: http://kucrut.org/
  * License: GPL v2
- * Text Domain: my-plugin
+ * Text Domain: menu-item-custom-fields-example
  */
 
 
@@ -30,12 +30,27 @@
 class Menu_Item_Custom_Fields_Example {
 
 	/**
+	 * Holds our custom fields
+	 *
+	 * @var    array
+	 * @access protected
+	 * @since  Menu_Item_Custom_Fields_Example 0.2.0
+	 */
+	protected static $fields = array();
+
+
+	/**
 	 * Initialize plugin
 	 */
 	public static function init() {
 		add_action( 'menu_item_custom_fields', array( __CLASS__, '_fields' ), 10, 3 );
 		add_action( 'wp_update_nav_menu_item', array( __CLASS__, '_save' ), 10, 3 );
 		add_filter( 'manage_nav-menus_columns', array( __CLASS__, '_columns' ), 99 );
+
+		self::$fields = array(
+			'field-01' => __( 'Custom Field #1', 'menu-item-custom-fields-example' ),
+			'field-02' => __( 'Custom Field #2', 'menu-item-custom-fields-example' ),
+		);
 	}
 
 
@@ -49,23 +64,31 @@ class Menu_Item_Custom_Fields_Example {
 	 * @param array $menu_item_args  Menu item data
 	 */
 	public static function _save( $menu_id, $menu_item_db_id, $menu_item_args ) {
+		if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
+			return;
+		}
+
 		check_admin_referer( 'update-nav_menu', 'update-nav-menu-nonce' );
 
-		// Sanitize
-		if ( ! empty( $_POST['menu-item-custom-field'][ $menu_item_db_id ] ) ) {
-			// Do some checks here...
-			$value = $_POST['menu-item-custom-field'][ $menu_item_db_id ];
-		}
-		else {
-			$value = '';
-		}
+		foreach ( self::$fields as $_key => $label ) {
+			$key = sprintf( 'menu-item-%s', $_key );
 
-		// Update
-		if ( ! empty( $value ) ) {
-			update_post_meta( $menu_item_db_id, 'menu-item-custom-field', $value );
-		}
-		else {
-			delete_post_meta( $menu_item_db_id, 'menu-item-custom-field' );
+			// Sanitize
+			if ( ! empty( $_POST[ $key ][ $menu_item_db_id ] ) ) {
+				// Do some checks here...
+				$value = $_POST[ $key ][ $menu_item_db_id ];
+			}
+			else {
+				$value = null;
+			}
+
+			// Update
+			if ( ! is_null( $value ) ) {
+				update_post_meta( $menu_item_db_id, $key, $value );
+			}
+			else {
+				delete_post_meta( $menu_item_db_id, $key );
+			}
 		}
 	}
 
@@ -81,30 +104,35 @@ class Menu_Item_Custom_Fields_Example {
 	 * @return string Form fields
 	 */
 	public static function _fields( $item, $depth, $args = array(), $id = 0 ) {
-		?>
-			<p class="field-custom description description-wide">
-				<label for="edit-menu-item-custom-field-<?php echo esc_attr( $item->ID ) ?>"><?php _e( 'Custom Field', 'my-plugin' ) ?><br />
+		foreach ( self::$fields as $_key => $label ) :
+			$key   = sprintf( 'menu-item-%s', $_key );
+			$id    = sprintf( 'edit-%s-%s', $key, $item->ID );
+			$name  = sprintf( '%s[%s]', $key, $item->ID );
+			$value = get_post_meta( $item->ID, $key, true );
+			$class = sprintf( 'field-%s', $_key );
+			?>
+				<p class="description description-wide <?php echo esc_attr( $class ) ?>">
 					<?php printf(
-						'<input type="text" value="%1$s" name="menu-item-custom-field[%2$d]" class="widefat code edit-menu-item-custom-field" id="edit-menu-item-custom-field-%2$d">',
-						esc_attr( get_post_meta( $item->ID, 'menu-item-custom-field', true ) ),
-						$item->ID
+						'<label for="%1$s">%2$s<br /><input type="text" id="%1$s" class="widefat %1$s" name="%3$s" value="%4$s" /></label>',
+						esc_attr( $id ),
+						esc_html( $label ),
+						esc_attr( $name ),
+						esc_attr( $value )
 					) ?>
-				</label>
-			</p>
-		<?php
+				</p>
+			<?php
+		endforeach;
 	}
 
 
 	/**
-	 * Add our field to the screen options toggle
-	 *
-	 * To make this work, the field wrapper must have the class 'field-custom'
+	 * Add our fields to the screen options toggle
 	 *
 	 * @param array $columns Menu item columns
 	 * @return array
 	 */
 	public static function _columns( $columns ) {
-		$columns['custom'] = __( 'Custom Field', 'my-plugin' );
+		$columns = array_merge( $columns, self::$fields );
 
 		return $columns;
 	}
