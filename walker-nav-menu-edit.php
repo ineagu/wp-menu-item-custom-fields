b15a4ca01cc5df5c501e26d9d9f0b5087b39cf07
Dzikri Aziz <kvcrvt@gmail.com>
2016-12-30 00:14:18 +0800

    Update walker's docblocks

--- walker-nav-menu-edit.php
+++ walker-nav-menu-edit.php
@@ -7,7 +7,7 @@
  * is only loaded on the wp-admin/nav-menus.php page.
  *
  * @package Menu_Item_Custom_Fields
- * @version 0.1.1
+ * @version 0.2.0
  * @author Dzikri Aziz <kvcrvt@gmail.com>
  */
 
@@ -27,6 +27,7 @@ class Menu_Item_Custom_Fields_Walker extends Walker_Nav_Menu_Edit {
 	 *
 	 * @see Walker_Nav_Menu::start_el()
 	 * @since 0.1.0
+	 * @since 0.2.0 Update regex pattern to support WordPress 4.7's markup.
 	 *
 	 * @param string $output Passed by reference. Used to append additional content.
 	 * @param object $item   Menu item data object.
